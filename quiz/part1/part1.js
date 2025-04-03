860643147025fcc072e4034d5d0d6895afb9500f
irsyad <irsyadnurhaakim40@gmail.c>
2025-04-03 16:44:27 +0700

    ujian part 2

---
+++ quiz/part1/part1.js
@@ -0,0 +1,136 @@
+
+// Kamu diminta untuk memprogram suatu game sederhana, Proxytia namanya. 
+// Untuk memulai game itu diperlukan 2 variabel (untuk sekarang), 
+// yaitu nama dan peran. Variabel peran harus memiliki isi data, 
+// bila nama kosong, pemain akan diberikan peringatan berupa "nama wajib diisi"
+// bila kosong pemain akan diberikan peringatan berupa "Pilih Peranmu untuk memulai game". 
+// Terdapat 3 peran berbeda yaitu Ksatria, Tabib, dan Penyihir. 
+// Tugas Anda adalah untuk membuat program yang mengecek isi variabel 
+// peran serta mengeluarkan respon sesuai isi variabel tersebut.
+//ada 4 jenis respons sesuai dengan 3 jenis peran yaitu
+//halo Ksatria ${nama} , kamu dapat menyerang dengan senjatamu!
+//halo Tabib ${nama} , kamu akan membantu temanmu yang terluka
+//halo Penyihir ${nama} , ciptakan keajaiban yang membantu kemenanganmu!
+//tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada
+//tips belajar penggunaan `` (backtick) pada javascript agar
+//mudah dalam memasukan variabel ke dalam string
+//tapi tanpa backtick juga ga masalah sih yg penting output sesuai
+
+// algoritma
+//isi algoritma mu disini (AWAS KALO GA DI ISI!!!!)
+
+
+let nama = "", peran = "";
+
+//code disini gunakan console.log untuk outputnya
+if (nama === "") {
+    console.log("Nama wajib diisi");
+}
+else if (peran === "") {
+    console.log("Pilih peranmu untuk memulai game");
+} else {
+    switch (peran) {
+        case "Ksatria":
+            console.log(`Halo Ksatria ${nama}, kamu dapat menyerang dengan senjatamu!`);
+            break;
+        case "Tabib":
+            console.log(`Halo Tabib ${nama}, kamu akan membantu temanmu yang terluka`);
+            break;
+        case "Penyihir":
+            console.log(`Halo Penyihir ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`);
+            break;
+        default:
+            console.log(`Halo ${peran} ${nama}, peran yang kamu pilih ga ada`);
+    }
+}
+```
+
+## Soal 2
+```js
+// Kamu akan diberikan sebuah tanggal dalam tiga variabel, yaitu hari, bulan, dan tahun. Disini kamu diminta untuk membuat format tanggal. Misal tanggal yang diberikan adalah hari 1, bulan 5, dan tahun 1945. Maka, output yang harus kamu proses adalah menjadi 1 Mei 1945.
+
+// Gunakan switch case untuk kasus ini!
+
+// Contoh:
+
+// let hari = 21; let bulan = 1; let tahun = 1945;
+
+// Maka hasil yang akan tampil di console adalah: '21 Januari 1945';
+
+//tips gunakan keyword ini di google "conditional switch case javascript"
+//dan mulailah membaca dan mencontek di stackoverflow xixixi
+
+let tanggal = 12; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+let bulan = "februari"; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+let tahun = 2001; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+
+//code switch case kamu disini
+switch (bulan) {
+    case "januari":
+        bulan = 1;
+        break;
+    case "februari":
+        bulan = 2;
+        break;
+    case "maret":
+        bulan = 3;
+        break;
+    case "april":
+        bulan = 4;
+        break;
+    case "mei":
+        bulan = 5;
+        break;
+    case "juni":
+        bulan = 6;
+        break;
+    case "juli":
+        bulan = 7;
+        break;
+    case "agustus":
+        bulan = 8;
+        break;
+    case "september":
+        bulan = 9;
+        break;  
+    case "oktober":
+        bulan = 10;
+        break;
+    case "november":
+        bulan = 11;
+        break;
+    case "desember":
+        bulan = 12;
+        break;
+    default:
+        console.log("Bulan tidak valid");
+        break;
+}
+
+if (bulan >= 1 && bulan <= 12 && tanggal >= 1 && tanggal <= 31 && tahun >= 1900 && tahun <= 2200) {
+    console.log(`${tanggal} ${bulan} ${tahun}`);
+} else {
+    console.log("Tanggal tidak valid");
+}
+let namaBulan;
+
+switch(bulan) {
+  case 1:
+    namaBulan = "januari";
+    break;
+  case 2:
+    namaBulan = "februari";
+    break;
+}
+if (namaBulan != "tanggaltidakvalid" && tanggal >= 1 && tanggal <= 31 && tahun >=1900 && tahun <= 2200) {
+  console.log(`${tanggal} ${namaBulan} ${tahun}`);
+} else {
+  console.log('tanggal tidak valid');
+}
+```
+---
+video biar rada paham lah, soalnya ini awal kepahitan ngoding wkwk:
+
+[Video tutor by Harkon](https://youtu.be/-YlMePibR6Y)
+
+*sisanya kalian googling aja ya ges yak*
---
+++ quiz/part2/part2.js
@@ -0,0 +1,114 @@
+//soal 1
+let word = "JavaScript";
+let second = "is";
+let third = "awesome";
+let fourth = "and";
+let fifth = "I";
+let sixth = "love";
+let seventh = "it!";
+
+//code here
+console.log(
+  word +
+    " " +
+    second +
+    " " +
+    third +
+    " " +
+    fourth +
+    " " +
+    fifth +
+    " " +
+    sixth +
+    " " +
+    seventh
+);
+```
+//soal 2
+```
+let word2 = 'wow JavaScript is so cool';
+let exampleFirstWord = word2[0] + word2[1] + word2[2];
+
+let exampleSecondtWord = word2[4] + word2[5] + word2[6] + word2[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13];
+console.log('Second Word: ' + exampleSecondtWord);
+let exampleThirdWord = word2[15] + word2[16] ;
+console.log('Third Word: ' + exampleThirdWord);
+let exampleFourthWord = word2[18] + word2[19] ;
+console.log('Fourth Word: ' + exampleFourthWord);
+let exampleFifthWord = word2[21] + word[222] + word2[23] + word2[24];
+console.log('Fifth Word: ' + exampleFifthWord);
+
+
+
+```
+// soal 3
+```
+let word3 = "wow JavaScript is so cool";
+let exampleFirstWord3 = word3.substring(0, 3);
+
+console.log("First Word: " + exampleFirstWord3);
+let word3 = "wow JavaScript is so cool";
+let exampleFirstWord3 = word3.substring(0, 3);
+
+console.log("First Word: " + exampleFirstWord3);
+
+let exampleSecondWord3 = word3.substring(4, 14);
+console.log("Second Word: " + exampleSecondWord3);
+let exampleThirdWord3 = word3.substring(15, 17);
+console.log("third Word: " + exampleThirdWord3);
+let exampleFourthWord3 = word3.substring(18, 20);
+console.log("Second Word: " + exampleFourthWord3);
+let exampleFifthWord3 = word3.substring(21, 25);
+console.log("Second Word: " + exampleFifthWord3);
+console.log(
+  exampleFirstWord3 +
+    " " +
+    exampleSecondWord3 +
+    " " +
+    exampleThirdWord3 +
+    " " +
+    exampleFourthWord3 +
+    " " +
+    exampleFifthWord3
+);
+```
+//soal 4
+```
+let word4 = "wow JavaScript is so cool";
+let exampleFirstWord4 = word4.substring(0, 3);
+
+let firstWordLength = exampleFirstWord4.length;
+
+console.log(
+  "First Word: " + exampleFirstWord + ", with length: " + firstWordLength
+);
+let exampleSecondWord4 = word4.substring(4,14);
+let secondWordLength = exampleSecondWord4.length;
+console.log(
+"First Word: " + exampleSecondWord4 + ", with length: " + secondWordLength)
+let exampleThirdWord4 = word4.substring(15,17);
+let thirdWordLength = exampleThirdWord4.length;
+console.log(
+  "First Word: " + exampleThirdWord4 + ", with length: " + thirdWordLength
+);
+let exampleFourthWord4 = word4.substring(18,20);
+let fourthWordLength = exampleFourthWord4.length;
+console.log(
+  "First Word: " + exampleFourthWord4 + ", with length: " + fourthWordLength
+);
+let exampleFifthWord4 = word4.substring(21,25);
+let fifthWordLength = exampleFifthWord4.length;
+console.log(
+  "First Word: " + exampleFifthWord4 + ", with length: " + fifthWordLength
+);
+console.log(
+  exampleFirstWord4 +
+    " " +
+    exampleSecondWord4 +
+    " " +
+    exampleThirdWord4 +
+    " " +
+    exampleFourthWord4 +
+    " " +
+    exampleFifthWord4
+);
